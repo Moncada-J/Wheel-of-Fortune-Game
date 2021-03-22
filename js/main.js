db9da13c310c7c78903d22d19016ba1caf8eac07
Jessalynn Moncada <moncadaj@Jessalynns-MacBook-Air.local>
2021-03-21 20:28:46 -0700

    First commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// ----------> VARIABLES < --------- 
+// create and assign constant (non changing) variables such as:
+//  number values within wheel & game play audio.
+
+// ----------->  GAME STATE  <------------
+// define the variables which are apart of the game state such as:
+// results, if player is a winner, how much the player has won or lost at the end.
+
+// --------------> ELEMENTS MANIPULATED  <---------------
+// based off game results, the modification of html elements such as:
+// player name, winner etc.
+
+// -----------> EVENT LISTENERS < -------------
+// the interaction between the user and the coded in elements such as:
+// buttons "clicking", audio etc.
+
+// -------------> THE FUNCTIONS OF THE GAME < -------------
+// the functioning of the game will include: a winning strategy using randomization, enabling a spinning 
+// effect  and tracking the amount of games played.
+
