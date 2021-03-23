f1653b7d2d3ec4c6e5f4e9f73e61c985aa3fccf7
Jessalynn Moncada <moncadaj@Jessalynns-MacBook-Air.local>
2021-03-23 16:30:39 -0700

    Addition of variables and event listeners

--- js/main.js
+++ js/main.js
@@ -3,23 +3,26 @@
 //  number values within wheel & game play audio.
 const wheelIndex = [0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10,  11];
 
-
 let spinAudio = new Audio('https://voca.ro/1hPceoBTH0Xk');
 
-let winAudio = new Audio('')
+let winAudio = new Audio('https://voca.ro/19yrhI3UJeKu');
 
 // ----------->  GAME STATE  <------------
 // define the variables which are apart of the game state such as:
 // results, if player is a winner, how much the player has won or lost at the end.
 
+let spins, result, winner;
+
 // --------------> ELEMENTS MANIPULATED  <---------------
 // based off game results, the modification of html elements such as:
 // player name, winner etc.
-
+const playerName = document.querySelector('#player-title');
+const winMoney = document.grabElementById('#credit-box');
+const loseMoney = document.grabElementById('#credit-box');
 // -----------> EVENT LISTENERS < -------------
 // the interaction between the user and the coded in elements such as:
 // buttons "clicking", audio etc.
-
+document.querySelector('.spin').addEventListener('click', spinWheel);
 // -------------> THE FUNCTIONS OF THE GAME < -------------
 // the functioning of the game will include: a winning strategy using randomization, enabling a spinning 
 // effect  and tracking the amount of games played.
