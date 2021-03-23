9189c257149009cdc2d3f797263d9f7dcef209d4
Jessalynn Moncada <moncadaj@Jessalynns-MacBook-Air.local>
2021-03-23 16:02:44 -0700

    audio added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 // ----------> VARIABLES < --------- 
 // create and assign constant (non changing) variables such as:
 //  number values within wheel & game play audio.
+const wheelIndex = [0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10,  11];
+
+
+let spinAudio = new Audio('https://voca.ro/1hPceoBTH0Xk');
+
+let winAudio = new Audio('')
 
 // ----------->  GAME STATE  <------------
 // define the variables which are apart of the game state such as:
